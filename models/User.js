f3f8672c3ee52b50ea7767a43d30b9d8c6d36cb0
Beatriz Lirios <beatrizplanejamento10@gmail.com>
2025-04-13 16:56:59 -0300

    Adicionando na model a senha automaticamente

--- models/User.js
+++ models/User.js
@@ -5,7 +5,21 @@ const UserSchema = new mongoose.Schema ({
     nome: {type: String, required: true}, //Nome do usuário (OBRIGATORIO)
     email:{type: String, required: true, unique: true}, //Email (OBRIGATORIO)
     senha:{type: String, required: true}, //Senha (será criptografada)
-    role: {type: String, enum: ['admin', 'atendente'], required: true},
+    role: {type: String, enum: ['admin', 'atendente'], default: 'atendente'},
 });
 
-module.exports = mongoose.model('User', UserSchema);
+//Middleware para criptografar a senha automaticamente antes de salvar 
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('senha')) return next();
+
+    try {
+        const salt = await bcrypt.genSalt(10);
+        thos.senha = await bcrypt.hash(this.senha, salt);
+        next();
+    }   catch (error) {
+        next(error);
+    }
+});
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
